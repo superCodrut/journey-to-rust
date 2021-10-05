599aa1728283c8f00d339ef22f81a428c0223450
Codrut Grosu <codrut.cristian.grosu@gmail.com>
2021-10-05 09:04:57 +0300

    stack_heap: add changes.

--- stack_heap/src/main.rs
+++ stack_heap/src/main.rs
@@ -1,3 +1,27 @@
+use std::mem;
+
+struct Point {
+    x:f64,
+    y:f64
+}
+
+fn origin () -> Point {
+    Point{x:0.0,y:0.0}
+}
+
+
+pub fn stack_and_heap() {
+
+}
+
 fn main() {
     println!("Hello, world!");
+    let x = origin();// stack allocation
+    let y = Box::new(origin());// heap
+    stack_and_heap();
+    println!("x takes {} bytes",mem::size_of_val(&x));
+    println!("y takes {} bytes",mem::size_of_val(&y));
+
+    let z = *y; // back on stack
+    println!("{}",z.x);
 }
