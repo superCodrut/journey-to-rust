0c4f0306ea11f2ef73431d492c8ab59689724e1c
Codrut Grosu <codrut.cristian.grosu@gmail.com>
2021-10-05 08:46:55 +0300

    scope_shadowing: add exercises.

--- scope_shadowing/src/main.rs
+++ scope_shadowing/src/main.rs
@@ -1,3 +1,35 @@
+use std::mem;
+
+const MOL:u8 = 42; // no fixed address
+
+static mol:i32 = 123;
+static mut mol1:i32 = 123;
+
+
+fn scope_and_shadowing() {
+
+    let a = 123;
+
+    {
+        let b = 10;
+        println!("inside, b = {}", b);
+        println!("inside, a = {}", a);
+        let a = b + 10;
+        println!("inside1, a = {}", a);
+
+    }
+
+
+    println!("a = {}", a);
+}
+
 fn main() {
     println!("Hello, world!");
+    scope_and_shadowing();
+    println!("{}",MOL);
+    println!("{}",mol);
+    unsafe {
+        mol1 = mol1 + 2;
+        println!("{}",mol1);
+    }
 }
